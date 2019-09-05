ea064359a0cb6981baea53446d892d943a03eb9a
Syed Abdul Samad <53892768+syedabdulsamad7@users.noreply.github.com>
2019-09-05 12:56:24 +0530

    Add files via upload

---
+++ 3a.py
@@ -0,0 +1,6 @@
+n=int(input("enter a number"))
+for i in range(1,int(n/2)+1):
+    if(n%i==0):
+        print(i)
+print(n)        
+        
---
+++ 3b.py
@@ -0,0 +1,23 @@
+import random
+for j in range(0,3):
+    s=" "
+    s=s+chr(random.randrange(97,123))
+    s=s+chr(random.randrange(65,91))
+    s=s+str(random.randrange(0,10))
+    s=s+chr(random.randrange(33,48))
+    l=random.randrange(10,16)
+    for i in range(4,l):
+        c=random.randrange(1,5)
+        if(c==1):
+           s=s+chr(random.randrange(97,123))
+        elif(c==2):
+            s=s+chr(random.randrange(65,91))
+        elif(c==3):
+            s=s+str(random.randrange(0,10))
+        else:
+            s=s+chr(random.randrange(33,48))
+    print(len(s))
+    print(s);
+    
+
+
